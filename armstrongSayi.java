e509fa97cb0cc42e655d92038b9fce7e50e43de7
“hava”7 <havvacigse@icloud.com>
2025-05-02 12:58:14 +0300

    Yeni dosyalar eklendi

--- armstrongSayi.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class armstrongSayi {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("bir sayi girin : ");
-        
-        
-    }
-}
