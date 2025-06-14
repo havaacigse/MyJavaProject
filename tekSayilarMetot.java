09f57d99f98e2c2d2bc0d242be994fcfc6e08342
“hava”7 <havvacigse@icloud.com>
2025-06-14 20:10:15 +0300

    Yeni dosyalar yüklendi

---
+++ tekSayilarMetot.java
@@ -0,0 +1,14 @@
+/* 11
+public class tekSayilarMetot {
+    public static void main(String[] args) {
+        int[] dizi = {3,5,7,9,45,34,23,24};
+    }
+    public static String[] tekleriGetir(int[] dizi){
+        for(int i=0 ; i<dizi.lenght ; i++){
+            if(i%2 == 0)
+               return
+        }
+
+    }
+}
+*/
