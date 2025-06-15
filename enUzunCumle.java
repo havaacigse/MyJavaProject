aa7faf3a51b6280e6e17dec3f90c7ac1a2f5cd10
“hava”7 <havvacigse@icloud.com>
2025-06-15 10:23:06 +0300

    Yeni dosyalar yüklendi

---
+++ enUzunCumle.java
@@ -0,0 +1,29 @@
+public class enUzunCumle {
+    public static void main(String[] args) {
+        System.out.println(enUzunCumle("merhaba arkadaslar " , "bugun cok guzel seyler olucak"));
+        System.out.println(enUzunCumle("yarın finallere calismam lazim " , "bugun denize gidicem."));
+    }
+    public static int enUzunCumle(String... cumleler){
+        if(cumleler == null || cumleler.length == 0)
+            return 0;
+         
+        int maxKelimeSayisi = 0;
+        String enUzunCumle = "";
+
+        for(String cumle : cumleler){
+            if(cumle == null || cumle.trim().isEmpty())
+               continue;
+            
+            String[] kelimeler = cumle.trim().split("\\s+");
+            int kelimeSayisi = kelimeler.length;
+
+            if(kelimeSayisi>maxKelimeSayisi){
+                maxKelimeSayisi = kelimeSayisi;
+                enUzunCumle = cumle;
+            }
+        }
+        System.out.println("en uzun cumle :" + enUzunCumle);
+            return maxKelimeSayisi;
+               
+    }
+}
