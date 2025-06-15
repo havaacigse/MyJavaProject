5648994f046f1b55123cb63b27790a0306d4406c
“hava”7 <havvacigse@icloud.com>
2025-06-15 10:32:38 +0300

    Yeni dosyalar yüklendi

---
+++ kuvvetAl.java
@@ -0,0 +1,14 @@
+public class kuvvetAl {
+    public static void main(String[] args) {
+        System.out.println(kuvvetAl(2, 5));
+        System.out.println(kuvvetAl(3, 4));
+        System.out.println(kuvvetAl(5, 0));
+        System.out.println(kuvvetAl(7, 2));
+    }
+    public static int kuvvetAl(int sayi , int kuvvet){
+        if(kuvvet == 0) 
+            return 1;
+        else 
+             return sayi * kuvvetAl(sayi, kuvvet -1);
+    }
+}
