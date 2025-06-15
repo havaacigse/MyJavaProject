f96a0588adcedbb619ccd90ff023ff32e7e6ca89
“hava”7 <havvacigse@icloud.com>
2025-06-15 10:41:49 +0300

    Yeni dosyalar yüklendi

---
+++ karelerinToplami.java
@@ -0,0 +1,22 @@
+public class karelerinToplami {
+    public static void main(String[] args) {
+        int sayi = 6;
+        int sonuc = karelerinToplami(sayi);
+
+        if(sonuc != -1){
+            System.out.println(sayi + sonuc);
+        }
+    }
+
+    public static int karelerinToplami(int sayi){
+        if(sayi<0){
+            System.out.println("hesaplanamaz");
+            return -1;
+        }
+        if(sayi == 1)
+             return 1;
+        else 
+             return (sayi * sayi) + karelerinToplami(sayi -1);
+
+    }
+}
